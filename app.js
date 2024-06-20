61cdca03c408988c9173d623f540e4b495f23d02
souheib-riache <souheib.riache@smartinia.com>
2024-06-20 08:52:40 +0200

    first commit project setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const data = require('./data/users.data')
+require('dotenv').config();
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(cors())
+
+app.get('/' , (req, res) => {
+    res.status(200).json(data.users)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on port ${PORT}`);
+})
---
+++ data/users.data.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        id: 1,
+        email: "souheib@gmail.com",
+        firstName: 'Souheib',
+        lastName: 'Riache'
+    },
+    {
+        id: 2,
+        email: "mohammed@gmail.com",
+        firstName: 'Mohammed',
+        lastName: 'Beciri'
+    },
+    {
+        id: 3,
+        email: "islam@gmail.com",
+        firstName: 'Islam',
+        lastName: 'Bousnina'
+    }
+]
+
+module.exports = {users}
